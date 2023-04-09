fed3a08e8fe6822cd470724a845ad21a408eadba
Ginkgozh <1813828837@qq.com>
2023-04-09 19:42:24 +0800

    test

---
+++ TinyQtServer/build/CMakeFiles/echo.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ginkgolinux/QtServerClient/TinyQtServer/main.cpp" "/home/ginkgolinux/QtServerClient/TinyQtServer/build/CMakeFiles/echo.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/ginkgolinux/chenshuo/build/release-install-cpp11/include"
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
